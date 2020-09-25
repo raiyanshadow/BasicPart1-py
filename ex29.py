fdd57fc7f5cd6e20485e18c66a5f657fbf3e8787
raiyanshadow <67802404+raiyanshadow@users.noreply.github.com>
2020-09-25 10:39:11 -0400

    Set operation name correction/Lazybin fix

--- ex29.py
+++ ex29.py
@@ -10,10 +10,7 @@ Expected Output
 
 '''
 
-color_list_1 = set(["White", "Black", "Red"])
-color_list_2 = set(["Red", "Green"])
-
-def delete(a, b):
-	return a.difference(b)
+color_set_1 = set(["White", "Black", "Red"])
+color_set_2 = set(["Red", "Green"])
 	
-print(delete(color_list_1, color_list_2))
+print(color_list_1.difference(color_list_2))
